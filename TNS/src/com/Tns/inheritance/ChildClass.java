53d3fcbee68cd8643a459d4d1a1a500fa7e01823
manisha449 <manimarch19@gmail.com>
2025-09-19 20:48:56 +0530

    inheritance

--- TNS/src/com/Tns/inheritance/ChildClass.java
+++ TNS/src/com/Tns/inheritance/ChildClass.java
@@ -1,5 +1,21 @@
 package com.Tns.inheritance;
 
-public class ChildClass {
+class ParentClass {
+    public void displayMessage() {
+        System.out.println("Message from ParentClass");
+    }
+}
 
+class ChildClass extends ParentClass {
+    @Override
+    public void displayMessage() {
+        // Call the displayMessage() method of the immediate parent class
+        super.displayMessage(); 
+        System.out.println("Message from ChildClass");
+    }
+
+    public static void main(String[] args) {
+        ChildClass child = new ChildClass();
+        child.displayMessage();
+    }
 }
