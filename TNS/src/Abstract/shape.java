4fc6dc53e4662b4a914bc83df741b54a792793af
manisha449 <manimarch19@gmail.com>
2025-09-20 14:12:02 +0530

    getset

--- TNS/src/Abstract/shape.java
+++ TNS/src/Abstract/shape.java
@@ -1,5 +1,9 @@
 package Abstract;
 
-public class shape {
-
+public abstract class shape {
+	protected float area;
+	abstract void calArea();
+	public void show() {
+		System.out.println("the area of shape is: "+area);
+	}
 }
