ad417bcec84d377212ec8e4a9917458c556f79be
manisha449 <manimarch19@gmail.com>
2025-09-22 08:02:49 +0530

    Array & strings

---
+++ TNS/src/ArrayString/InterfaceExtendExmaple.java
@@ -0,0 +1,34 @@
+package ArrayString;
+
+	//Parent Interface
+	interface Animal {
+	 void eat();   // abstract method
+	}
+
+	//Child Interface extending parent
+	interface Dog extends Animal {
+	 void bark();  // abstract method
+	}
+
+	//Implementation class
+	class Puppy implements Dog {
+	 @Override
+	 public void eat() {
+	     System.out.println("Dog is eating...");
+	 }
+
+	 @Override
+	 public void bark() {
+	     System.out.println("Dog barks...");
+	 }
+	}
+
+	public class InterfaceExtendExample {
+	 public static void main(String[] args) {
+	     // Using interface reference
+	     Dog d = new Puppy();
+
+	     d.eat();   // from Animal interface
+	     d.bark();  // from Dog interface
+	 }
+	}
---
+++ TNS/src/com/Manisha/assignment/utilities/AssignmentMain.java
@@ -0,0 +1,5 @@
+package com.Manisha.assignment.utilities;
+
+public class AssignmentMain {
+
+}
