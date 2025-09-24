56a07299d05040a3f0d487fc8f8ee38de9d49e2c
manisha449 <manimarch19@gmail.com>
2025-09-24 19:55:25 +0530

    Task

--- TNS/src/com/Tns/ArrayString/Wrapper.java
+++ TNS/src/com/Tns/ArrayString/Wrapper.java
@@ -2,4 +2,23 @@ package com.Tns.ArrayString;
 
 public class Wrapper {
 
-}
+		public static void main(String[] args) {
+			
+					int num =5;
+					Integer num1 = num;
+					
+					double dob = 5.0;
+					Double dob1 = dob;
+					
+					char ch = 'A';
+					Character ch1 = ch;
+				
+					int num2 = num1;
+					double dob2 = dob1;
+					char ch2 = ch1;
+					System.out.println("Auto-Boxing Variables:"+num1+" "+" "+dob1+" "+ch1);
+					System.out.println("Auto-UnBoxing Variables:"+num2+" "+" "+dob2+" "+ch2);
+					
+		}
+
+	}
